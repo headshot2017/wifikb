767d2ddaffc2878a111b16582cc6c4624b51f0e5
headshot2017 <>
2024-11-29 00:30:52 -0400

    test program: allow choosing reverse mode
    also fix tabbing

--- source/test.cpp
+++ source/test.cpp
@@ -15,46 +15,67 @@
 int main(int argc, char *argv[])
 {
 	PrintConsole topScreen;
-    PrintConsole bottomScreen;
+	PrintConsole bottomScreen;
 
 	videoSetMode(MODE_0_2D);
-    videoSetModeSub(MODE_0_2D);
+	videoSetModeSub(MODE_0_2D);
 
-    vramSetBankA(VRAM_A_MAIN_BG);
-    vramSetBankC(VRAM_C_SUB_BG);
+	vramSetBankA(VRAM_A_MAIN_BG);
+	vramSetBankC(VRAM_C_SUB_BG);
 
-    consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
-    consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+	consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+	consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
 
-    consoleSelect(&topScreen);
+	consoleSelect(&topScreen);
 
-    printf("Connecting to WiFi using\n");
-    printf("firmware settings...\n\n");
+	printf("Connecting to WiFi using\n");
+	printf("firmware settings...\n\n");
 
-    if (!Wifi_InitDefault(WFC_CONNECT))
+	if (!Wifi_InitDefault(WFC_CONNECT))
 	{
-        printf("Can't connect to WiFi!\n");
+		printf("Can't connect to WiFi!\n");
 		while (1) swiWaitForVBlank();
 	}
 	else
 	{
-        printf("Connected!\n");
+		printf("Connected!\n");
 
 		struct in_addr ip, gateway, mask, dns1, dns2;
-        ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
+		ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
 
-        printf("\n");
-        printf("Connection information:\n");
-        printf("\n");
-        printf("IP:      %s\n", inet_ntoa(ip));
-        printf("Gateway: %s\n", inet_ntoa(gateway));
-        printf("Mask:    %s\n", inet_ntoa(mask));
-        printf("DNS1:    %s\n", inet_ntoa(dns1));
-        printf("DNS2:    %s\n", inet_ntoa(dns2));
+		printf("\n");
+		printf("Connection information:\n");
+		printf("\n");
+		printf("IP:      %s\n", inet_ntoa(ip));
+		printf("Gateway: %s\n", inet_ntoa(gateway));
+		printf("Mask:    %s\n", inet_ntoa(mask));
+		printf("DNS1:    %s\n", inet_ntoa(dns1));
+		printf("DNS2:    %s\n", inet_ntoa(dns2));
+		printf("\n");
+	}
+
+	printf("Enable reverse connection mode?\n");
+	printf("This allows connecting to\n");
+	printf("emulators.\n");
+	printf("A: Yes\n");
+	printf("B: No\n");
+
+	while (1)
+	{
+		swiWaitForVBlank();
+
+		scanKeys();
+		u32 key = keysDown();
+
+		if (key & (KEY_A | KEY_B))
+		{
+			wifikb::setReverse(!!(key & KEY_A));
+			printf("Reverse mode %sabled\nWaiting for connection now\n", key&KEY_A ? "en" : "dis");
+			break;
+		}
 	}
 
 	consoleSelect(&bottomScreen);
-	wifikb::setReverse(true);
 	wifikb::init();
 
 	wifikb::start();
